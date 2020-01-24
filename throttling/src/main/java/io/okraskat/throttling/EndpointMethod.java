209c8402da7abfdd606338dc8689efc5a84f49bf
Tomasz Okraska <tokraska@billtech.pl>
2020-01-24 10:15:48 +0100

    throttling example - add equals hashcode

--- throttling/src/main/java/io/okraskat/throttling/EndpointMethod.java
+++ throttling/src/main/java/io/okraskat/throttling/EndpointMethod.java
@@ -1,5 +1,8 @@
 package io.okraskat.throttling;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 class EndpointMethod {
 
     private final Class targetClass;
@@ -16,5 +19,27 @@ class EndpointMethod {
 
     public Class getTargetClass() {
         return targetClass;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        EndpointMethod that = (EndpointMethod) o;
+
+        return new EqualsBuilder()
+                .append(targetClass, that.targetClass)
+                .append(targetMethod, that.targetMethod)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(targetClass)
+                .append(targetMethod)
+                .toHashCode();
     }
 }
