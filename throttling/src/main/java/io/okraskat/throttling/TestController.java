b89e19147922034e07c5c9d1e07f2e7a4b172b5e
Tomasz Okraska <tokraska@billtech.pl>
2020-02-17 09:23:16 +0100

    throttling example - add call counter

--- throttling/src/main/java/io/okraskat/throttling/TestController.java
+++ throttling/src/main/java/io/okraskat/throttling/TestController.java
@@ -4,13 +4,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController("/test")
 public class TestController {
+    private final AtomicInteger atomicInteger = new AtomicInteger(0);
 
     @GetMapping
     @Throttling(timeFrameInSeconds = 60, calls = 2)
     public String exampleEndpoint() {
+        System.out.println("Call number: " + atomicInteger.incrementAndGet());
         return UUID.randomUUID().toString();
     }
 }
